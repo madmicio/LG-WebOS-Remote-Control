235c1eea538a67b1c225181eb07fe83b993b390c
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2020-04-23 10:23:34 +0200

    Added possibility to use disney/dazn icon in sources configuration

--- lg-remote-control.js
+++ lg-remote-control.js
@@ -1,10 +1,71 @@
-import {
-    LitElement,
-    html,
-    css
-} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
+var LitElement = LitElement || Object.getPrototypeOf(customElements.get("home-assistant-main"));
+var html = html || LitElement.prototype.html;
+var css = css || LitElement.prototype.css;
 
 class TestLgRemoteControl extends LitElement {
+
+    static disneyIcon = html`<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                  width="21.000000pt" height="21.000000pt" viewBox="0 0 122.000000 125.000000"
+                  preserveAspectRatio="xMidYMid meet">
+                 <metadata>
+                 Created by potrace 1.15, written by Peter Selinger 2001-2017
+                 </metadata>
+                 <g transform="translate(0.000000,125.000000) scale(0.100000,-0.100000)"
+                 fill="var(--primary-text-color)" stroke="none">
+                 <path d="M841 1181 c-14 -9 -12 -53 4 -107 6 -18 -1 -24 -50 -44 -50 -20 -56
+                 -26 -53 -49 4 -33 34 -39 89 -17 22 9 41 16 43 16 3 0 12 -23 21 -50 19 -58
+                 43 -76 71 -56 17 13 17 19 7 58 -6 23 -14 50 -18 59 -4 12 6 20 42 33 70 25
+                 78 31 78 57 0 31 -38 37 -102 15 -26 -9 -47 -16 -48 -16 -1 0 -8 19 -14 43
+                 -20 64 -37 79 -70 58z"/>
+                 <path d="M293 805 c-113 -31 -173 -77 -173 -132 0 -34 8 -38 45 -23 19 7 19 8
+                 -5 18 l-25 9 33 20 c61 37 171 56 295 50 127 -6 203 -23 297 -69 115 -56 200
+                 -165 200 -257 0 -39 -38 -107 -76 -136 -75 -57 -200 -99 -221 -73 -6 7 -22 49
+                 -36 93 -14 44 -28 88 -32 97 -9 23 55 40 133 36 l57 -3 -3 -32 c-4 -40 18 -45
+                 42 -11 24 33 20 71 -10 99 -22 20 -35 24 -92 24 -37 0 -86 -4 -109 -8 l-43 -9
+                 -23 54 c-13 29 -34 62 -46 72 -21 19 -21 19 -21 -11 0 -16 5 -56 12 -88 l11
+                 -58 -29 -20 c-68 -48 -114 -104 -114 -136 0 -51 71 -106 188 -147 39 -13 58
+                 -27 72 -51 11 -18 24 -33 29 -33 24 0 41 14 41 35 0 20 7 24 59 33 173 31 293
+                 163 278 308 -13 129 -112 232 -290 304 -146 58 -326 76 -444 45z m259 -502
+                 l25 -88 -33 -3 c-37 -3 -140 43 -158 70 -8 13 -6 22 9 38 20 23 101 69 121 70
+                 6 0 22 -39 36 -87z"/>
+                 </g>
+                 </svg>`;
+    static daznIcon = html`<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                    width="22.000000pt" height="20.000000pt" viewBox="0 0 324.000000 323.000000"
+                    preserveAspectRatio="xMidYMid meet">
+                  <metadata>
+                  Created by potrace 1.15, written by Peter Selinger 2001-2017
+                  </metadata>
+                  <g transform="translate(0.000000,323.000000) scale(0.09000,-0.090000)"
+                  fill="var(--primary-text-color)"  stroke="none">
+                  <path d="M197 3033 c-4 -3 -7 -286 -7 -627 l0 -621 87 -87 87 -88 -87 -88 -87
+                  -89 0 -627 0 -626 1430 0 1430 0 0 627 0 628 -87 88 -88 88 88 86 87 86 0 628
+                  0 629 -1423 0 c-783 0 -1427 -3 -1430 -7z m2713 -663 l0 -531 -115 -117 -115
+                  -117 115 -115 115 -115 0 -528 0 -527 -1290 0 -1290 0 0 527 0 528 117 117
+                  118 118 -118 118 -117 117 0 521 c0 286 3 524 7 527 3 4 584 7 1290 7 l1283 0
+                  0 -530z"/>
+                  <path d="M800 2175 l0 -395 238 0 237 0 87 88 87 87 1 219 1 219 -88 89 -88
+                  88 -237 0 -238 0 0 -395z m462 208 l48 -47 0 -160 0 -160 -47 -48 -47 -48
+                  -138 0 -138 0 0 248 c0 137 3 252 7 255 3 4 65 7 137 7 l130 0 48 -47z"/>
+                  <path d="M2052 2553 c-10 -18 -68 -157 -214 -508 -43 -104 -85 -205 -93 -223
+                  -8 -18 -15 -35 -15 -37 0 -3 34 -5 75 -5 l74 0 22 50 21 50 198 0 197 0 21
+                  -50 21 -50 77 0 c57 0 75 3 71 13 -3 6 -21 48 -40 92 -31 73 -69 164 -230 550
+                  l-54 130 -61 3 c-50 2 -62 0 -70 -15z m141 -375 l65 -158 -139 0 -139 0 20 48
+                  c11 26 41 100 67 164 26 64 51 114 55 110 3 -4 35 -78 71 -164z"/>
+                  <path d="M806 1433 c-3 -3 -6 -37 -6 -74 l0 -68 205 2 c113 1 205 1 205 -1 0
+                  -2 -37 -49 -82 -104 -46 -55 -94 -115 -108 -132 -13 -17 -68 -84 -122 -148
+                  l-98 -118 0 -70 0 -70 300 0 300 0 0 70 0 70 -205 0 c-113 0 -205 2 -205 5 0
+                  3 9 15 19 27 11 13 103 124 205 248 l186 225 -3 70 -2 70 -291 2 c-160 1 -294
+                  -1 -298 -4z"/>
+                  <path d="M1795 1432 c-3 -3 -5 -180 -5 -394 l0 -388 70 0 70 0 2 252 3 251
+                  175 -251 175 -251 78 -1 77 0 0 394 0 395 -67 -2 -68 -2 -5 -276 -5 -276 -195
+                  279 -195 279 -52 -2 c-29 -1 -55 -5 -58 -7z"/>
+                  </g>
+                  </svg>`;
+    static iconMapping = {
+        "disney": TestLgRemoteControl.disneyIcon,
+        "dazn": TestLgRemoteControl.daznIcon
+    };
 
     static get properties() {
         return {
@@ -19,12 +80,11 @@ class TestLgRemoteControl extends LitElement {
     }
 
     render() {
-        const defaultSource = this.config.defaultsource === "enable";    // <= this variable does not work! :(
-        const config = this.config;
         const stateObj = this.hass.states[this.config.entity];
+        const cardHeight = 750 + (this.config.sources ? Math.floor(this.config.sources.length/4) * 42 : 0);
         return html`
         <div class="card">
-        <div class="page">
+        <div class="page" style="height: ${cardHeight}px">
               <div class="grid-container">
                 <div class="shape">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80"><path d="m 30 15 a 10 10 0 0 1 20 0 a 15 15 0 0 0 15 15 a 10 10 0 0 1 0 20 a 15 15 0 0 0 -15 15 a 10 10 0 0 1 -20 0 a 15 15 0 0 0 -15 -15 a 10 10 0 0 1 0 -20 a 15 15 0 0 0 15 -15" fill="var(--deactive-background-button-color)" stroke="#000000" stroke-width="0" /></svg>
@@ -67,8 +127,20 @@ class TestLgRemoteControl extends LitElement {
                 </div> 
 
               </div>
-
-              ${defaultSource ? html`
+ 
+              ${this.config.sources ? html`
+                <div class="grid-container-source">
+                ${this.config.sources.map(source => {
+            return html`
+                  <div class="grid-item">
+                    <button class="btn_source ripple" @click=${() => this._select_source(source.name)}>
+                      ${TestLgRemoteControl.getIcon(source.icon)}
+                    </button>
+                  </div>
+                  `;
+        })}
+                </div>
+                ` : html`
               <div class="grid-container-source">
                 <div class="grid-item netflix">
                   <button class="btn_source ripple" @click=${() => this._netflix()}><ha-icon icon="mdi:netflix"/></button>
@@ -77,99 +149,12 @@ class TestLgRemoteControl extends LitElement {
                   <button class="btn_source ripple" @click=${() => this._amazon()}><ha-icon icon="mdi:amazon"/></button>
                 </div>
                 <div class="grid-item disney">
-                  <button class="btn_source ripple" @click=${() => this._disney()}><svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                  width="21.000000pt" height="21.000000pt" viewBox="0 0 122.000000 125.000000"
-                  preserveAspectRatio="xMidYMid meet">
-                 <metadata>
-                 Created by potrace 1.15, written by Peter Selinger 2001-2017
-                 </metadata>
-                 <g transform="translate(0.000000,125.000000) scale(0.100000,-0.100000)"
-                 fill="var(--primary-text-color)" stroke="none">
-                 <path d="M841 1181 c-14 -9 -12 -53 4 -107 6 -18 -1 -24 -50 -44 -50 -20 -56
-                 -26 -53 -49 4 -33 34 -39 89 -17 22 9 41 16 43 16 3 0 12 -23 21 -50 19 -58
-                 43 -76 71 -56 17 13 17 19 7 58 -6 23 -14 50 -18 59 -4 12 6 20 42 33 70 25
-                 78 31 78 57 0 31 -38 37 -102 15 -26 -9 -47 -16 -48 -16 -1 0 -8 19 -14 43
-                 -20 64 -37 79 -70 58z"/>
-                 <path d="M293 805 c-113 -31 -173 -77 -173 -132 0 -34 8 -38 45 -23 19 7 19 8
-                 -5 18 l-25 9 33 20 c61 37 171 56 295 50 127 -6 203 -23 297 -69 115 -56 200
-                 -165 200 -257 0 -39 -38 -107 -76 -136 -75 -57 -200 -99 -221 -73 -6 7 -22 49
-                 -36 93 -14 44 -28 88 -32 97 -9 23 55 40 133 36 l57 -3 -3 -32 c-4 -40 18 -45
-                 42 -11 24 33 20 71 -10 99 -22 20 -35 24 -92 24 -37 0 -86 -4 -109 -8 l-43 -9
-                 -23 54 c-13 29 -34 62 -46 72 -21 19 -21 19 -21 -11 0 -16 5 -56 12 -88 l11
-                 -58 -29 -20 c-68 -48 -114 -104 -114 -136 0 -51 71 -106 188 -147 39 -13 58
-                 -27 72 -51 11 -18 24 -33 29 -33 24 0 41 14 41 35 0 20 7 24 59 33 173 31 293
-                 163 278 308 -13 129 -112 232 -290 304 -146 58 -326 76 -444 45z m259 -502
-                 l25 -88 -33 -3 c-37 -3 -140 43 -158 70 -8 13 -6 22 9 38 20 23 101 69 121 70
-                 6 0 22 -39 36 -87z"/>
-                 </g>
-                 </svg></button>
+                  <button class="btn_source ripple" @click=${() => this._disney()}>${TestLgRemoteControl.disneyIcon}</button>
                 </div>
                 <div class="grid-item dazn">
-                  <button class="btn_source ripple" @click=${() => this._dazn()}><svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                    width="22.000000pt" height="20.000000pt" viewBox="0 0 324.000000 323.000000"
-                    preserveAspectRatio="xMidYMid meet">
-                  <metadata>
-                  Created by potrace 1.15, written by Peter Selinger 2001-2017
-                  </metadata>
-                  <g transform="translate(0.000000,323.000000) scale(0.09000,-0.090000)"
-                  fill="var(--primary-text-color)"  stroke="none">
-                  <path d="M197 3033 c-4 -3 -7 -286 -7 -627 l0 -621 87 -87 87 -88 -87 -88 -87
-                  -89 0 -627 0 -626 1430 0 1430 0 0 627 0 628 -87 88 -88 88 88 86 87 86 0 628
-                  0 629 -1423 0 c-783 0 -1427 -3 -1430 -7z m2713 -663 l0 -531 -115 -117 -115
-                  -117 115 -115 115 -115 0 -528 0 -527 -1290 0 -1290 0 0 527 0 528 117 117
-                  118 118 -118 118 -117 117 0 521 c0 286 3 524 7 527 3 4 584 7 1290 7 l1283 0
-                  0 -530z"/>
-                  <path d="M800 2175 l0 -395 238 0 237 0 87 88 87 87 1 219 1 219 -88 89 -88
-                  88 -237 0 -238 0 0 -395z m462 208 l48 -47 0 -160 0 -160 -47 -48 -47 -48
-                  -138 0 -138 0 0 248 c0 137 3 252 7 255 3 4 65 7 137 7 l130 0 48 -47z"/>
-                  <path d="M2052 2553 c-10 -18 -68 -157 -214 -508 -43 -104 -85 -205 -93 -223
-                  -8 -18 -15 -35 -15 -37 0 -3 34 -5 75 -5 l74 0 22 50 21 50 198 0 197 0 21
-                  -50 21 -50 77 0 c57 0 75 3 71 13 -3 6 -21 48 -40 92 -31 73 -69 164 -230 550
-                  l-54 130 -61 3 c-50 2 -62 0 -70 -15z m141 -375 l65 -158 -139 0 -139 0 20 48
-                  c11 26 41 100 67 164 26 64 51 114 55 110 3 -4 35 -78 71 -164z"/>
-                  <path d="M806 1433 c-3 -3 -6 -37 -6 -74 l0 -68 205 2 c113 1 205 1 205 -1 0
-                  -2 -37 -49 -82 -104 -46 -55 -94 -115 -108 -132 -13 -17 -68 -84 -122 -148
-                  l-98 -118 0 -70 0 -70 300 0 300 0 0 70 0 70 -205 0 c-113 0 -205 2 -205 5 0
-                  3 9 15 19 27 11 13 103 124 205 248 l186 225 -3 70 -2 70 -291 2 c-160 1 -294
-                  -1 -298 -4z"/>
-                  <path d="M1795 1432 c-3 -3 -5 -180 -5 -394 l0 -388 70 0 70 0 2 252 3 251
-                  175 -251 175 -251 78 -1 77 0 0 394 0 395 -67 -2 -68 -2 -5 -276 -5 -276 -195
-                  279 -195 279 -52 -2 c-29 -1 -55 -5 -58 -7z"/>
-                  </g>
-                  </svg></button>
+                  <button class="btn_source ripple" @click=${() => this._dazn()}>${TestLgRemoteControl.daznIcon}</button>
                 </div>
-              </div>
-
-              ` : html``}
-                ${this.config.sources ? html`
-                <div class="grid-container-source">
-                ${this.config.sources.map(source => {return html`
-                  <div class="grid-item">
-                    <button class="btn_source ripple" @click=${() => this._select_source(source.name)}><ha-icon icon="${source.icon}"/></button>
-                  </div>
-                `;})}
-                </div>
-                ` : html``}
-
-                ${defaultSource ? html`
-                <div class="grid-container-source">
-                  <div class="grid-item netflix">
-                    <button class="btn_source ripple" @click=${() => this._netflix()}><ha-icon icon="${config.source[0].icon}"/></button>
-                  </div>
-                  <div class="grid-item amazon">
-                    <button class="btn_source ripple" @click=${() => this._amazon()}><ha-icon icon="mdi:amazon"/></button>
-                  </div>
-                  <div class="grid-item netflix">
-                    <button class="btn_source ripple" @click=${() => this._netflix()}><ha-icon icon="mdi:netflix"/></button>
-                  </div>
-                  <div class="grid-item amazon">
-                    <button class="btn_source ripple" @click=${() => this._amazon()}><ha-icon icon="mdi:amazon"/></button>
-                  </div>
-                  
-                </div>
-  
-                ` : html``}
-
+              </div>`}
 
               <div class="grid-container-bottom">
                 <div class="grid-item">
@@ -489,6 +474,12 @@ class TestLgRemoteControl extends LitElement {
 
     getCardSize() {
         return 2;
+    }
+
+    static getIcon(iconName) {
+        return Object.keys(this.iconMapping).includes(iconName)
+            ? this.iconMapping[iconName]
+            : html`<ha-icon icon="${iconName}"/>`;
     }
 
     static get styles() {
